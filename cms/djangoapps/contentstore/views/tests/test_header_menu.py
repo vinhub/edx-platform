d2b9e890d0c95bf6eb4cc35de906e63a0502296d
Zia Fazal <zia.fazal@arbisoft.com>
2015-10-29 13:43:39 +0500

    enabled web certs by default
    changing default value instead of setting it via fields
    
    fixed broken test
    
    fixed quality violation

--- cms/djangoapps/contentstore/views/tests/test_header_menu.py
+++ cms/djangoapps/contentstore/views/tests/test_header_menu.py
@@ -29,6 +29,8 @@ class TestHeaderMenu(CourseTestCase):
         Tests course header menu should not have `Certificates` menu item
         if course has not web/HTML certificates enabled.
         """
+        self.course.cert_html_view_enabled = False
+        self.save_course()
         outline_url = reverse_course_url('course_handler', self.course.id)
         resp = self.client.get(outline_url, HTTP_ACCEPT='text/html')
         self.assertEqual(resp.status_code, 200)
@@ -39,8 +41,6 @@ class TestHeaderMenu(CourseTestCase):
         Tests course header menu should have `Certificates` menu item
         if course has web/HTML certificates enabled.
         """
-        self.course.cert_html_view_enabled = True
-        self.save_course()
         outline_url = reverse_course_url('course_handler', self.course.id)
         resp = self.client.get(outline_url, HTTP_ACCEPT='text/html')
         self.assertEqual(resp.status_code, 200)
